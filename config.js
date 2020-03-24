9252d3c3eb594e9092706a302137f250823a2bb8
Mario Torres <mario@zooft.company>
2020-03-24 17:16:07 -0300

    fix config.js and create error not found

--- config.js
+++ config.js
@@ -29,8 +29,7 @@ config = _.merge({}, {
   DB_PORT: process.env.DB_PORT,
   DB_USER: process.env.DB_USER,
   DB_PASS: process.env.DB_PASS,
-  DB_NAME: process.env.DB_NAME,
-  SECRET_KEY: process.env.SECRET_KEY
+  DB_NAME: process.env.DB_NAME
 }, config);
 
 
